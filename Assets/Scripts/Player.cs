5a970d423443e880a0886f10214aedeb333fbe4f
Tashad <tashadurrahman1924@gmail.com>
2025-12-12 22:59:24 +0530

    Added animator controls for walk and idle state

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,24 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float _jumpEndTime;
-    [SerializeField] private float _horizontalVelocity = 3f;
-    [SerializeField] private float _jumpVelocity = 5f;
-    [SerializeField] private float _jumpDuration = 0.5f;
-    public bool isGrounded;
+    float _jumpEndTime;
+    [SerializeField] float _horizontalVelocity = 3f;
+    [SerializeField] float _jumpVelocity = 5f;
+    [SerializeField] float _jumpDuration = 0.5f;
+    [SerializeField] Sprite _jumpSprite;
+    public bool IsGrounded;   // CapitalCase for global variables
+    Sprite _defaultSprite;
+    SpriteRenderer _spriteRenderer;
+    private float _horizontal;
+
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _defaultSprite = _spriteRenderer.sprite;
+    }
 
     void OnDrawGizmos()
     {
@@ -23,31 +34,38 @@ public class Player : MonoBehaviour
     {
         // Debug.Log("Updated at " + Time.time);  // get time elapsed since game started
 
-        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - _spriteRenderer.bounds.extents.y);
         var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f);
 
         if (hit.collider)
-            isGrounded = true;
-        else 
-            isGrounded = false;
-        
-        var horizontal = Input.GetAxis("Horizontal");
-        // Debug.Log(horizontal);
+            IsGrounded = true;
+        else
+            IsGrounded = false;
+
+        _horizontal = Input.GetAxis("Horizontal");
+        // Debug.Log(_horizontal);
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         var vertical = rb.linearVelocityY;
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
+        if (Input.GetButtonDown("Jump") && IsGrounded)
             _jumpEndTime = Time.time + _jumpDuration;
-        }
 
-        if(Input.GetButton("Jump") && _jumpEndTime > Time.time)
-        {
+        if (Input.GetButton("Jump") && _jumpEndTime > Time.time)
             vertical = _jumpVelocity;
-        }
 
-        horizontal *= _horizontalVelocity;
-        rb.linearVelocity = new Vector2(horizontal, vertical);  // In Unity, this is the way to set velocity to a RB, we can't set horizontal and vertical velocity separately
+        _horizontal *= _horizontalVelocity;
+        rb.linearVelocity = new Vector2(_horizontal, vertical);  // In Unity, this is the way to set velocity to a RB, we can't set horizontal and vertical velocity separately
+        UpdateSprite();
+    }
+
+    void UpdateSprite()
+    {
+        GetComponent<Animator>().SetBool("IsGrounded", IsGrounded);
+        GetComponent<Animator>().SetFloat("HorizontalSpeed", Math.Abs(_horizontal));
+
+        if(_horizontal > 0)
+            _spriteRenderer.flipX = false;
+        else if(_horizontal < 0)    // reason for using else if (not just else) is so that if _horizontal=0 then it stays in same flipped state as it was
+            _spriteRenderer.flipX = true;
     }
 }
